843cf6ddfdb6e63cd1234c9de35b869e2ffd685c
Casper Shih <casshih@gmail.com>
2018-09-01 08:40:44 -0700

    folder setup

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+
+const header = props => (
+    <header className="header">
+        <h1></h1>  
+        <h2></h2>
+    </header>
+);
+
+export default header;
