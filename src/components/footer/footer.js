eecf36cd7d7662e2db560b3dd6a9fa7832af0e97
Casper Shih <casshih@gmail.com>
2018-09-09 11:27:47 -0700

    heroku push

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+const footer = () => (
+    <footer className="footer">
+        <div>
+            <p>Clicky</p>
+        </div>
+    </footer>
+);
+
+export default footer;
